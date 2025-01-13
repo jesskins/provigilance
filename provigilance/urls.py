14ce3e43fe8b55b98da880554cf7bfe9c31ae155
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-13 20:28:17 +0000

    may have finally found the problem??

--- provigilance/urls.py
+++ provigilance/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from about.views import my_about
+from about.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
