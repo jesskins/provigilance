495058b90e9da73dcaa1e4c95feb111f0336df80
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-15 16:59:14 +0000

    changed views.py to ref to submit

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.core.mail import send_mail
-from oauth2_provider.models import AccessToken
 from .models import Testimonial
 from .forms import TestimonialForm
+import subprocess
+import os
 
 # Create your views here.
 
@@ -15,34 +15,21 @@ def submit_testimonial(request):
     if request.method == 'POST':
         form = TestimonialForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            # Save the form and get the testimonial instance
+            testimonial = form.save()
 
-            # Extract email and name from the form 
-            recipient_email = testimonial.email 
-            recipient_name = testimonial.name 
-            # Call the send_email.py script with the recipient's email and name 
-            script_path = os.path.join(os.path.dirname(__file__), 'send_email.py') 
+            # Extract email and name from the form
+            recipient_email = testimonial.email
+            recipient_name = testimonial.name
+
+            # Call the send_email.py script with the recipient's email and name
+            script_path = os.path.join(os.path.dirname(__file__), 'send_email.py')
             subprocess.call(['python', script_path, recipient_email, recipient_name])
-            
-            return redirect('submit_success')
 
+            return redirect('submit_success')
     else:
         form = TestimonialForm()
     return render(request, 'testimonials/submit_testimonial.html', {'form': form})
 
 def submit_success(request):
     return render(request, 'testimonials/submit_success.html')
-
-
-
-# unsure if i will need these later, keeping in case
-
-#def index(request):
- #   return render(request, 'testimonials/testimonials_list.html')
-
-#def index(request):
- #   if request.method == "POST":
-  #      return HttpResponse("You must have POSTed something")
-   # else:
-    #    return HttpResponse(request.method)
-
