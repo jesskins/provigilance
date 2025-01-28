790b33d87c899e2ec0e9ea0f24d9424f4a094018
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-28 00:34:48 +0000

    pep8 debugging

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Testimonial
 
+
 @admin.register(Testimonial)
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('name', 'company', 'email', 'service_month', 'service_year', 'recommend', 'created_at')
+    list_display = ('name', 'company', 'email', 'service_month', \
+    'service_year', 'recommend', 'created_at')
     list_filter = ('service_year', 'recommend')
     search_fields = ('name', 'company', 'email', 'text')
