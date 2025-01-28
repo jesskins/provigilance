7337b8d52f33f676c42c74d46c5b511e4b5df821
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-28 10:53:33 +0000

    python in about/ checked for errors

--- about/views.py
+++ about/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render 
-#from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
-def about(request): 
-  return render(request, 'about.html')
+def about(request):
+    return render(request, 'about.html')
+
 
 def home(request):
     return render(request, 'about.html')
 
-def privacy_policy(request): 
-  return render(request, 'privacy_policy.html')
 
-#def about(request):
- #   return render(request, 'about.html')
+def privacy_policy(request):
+    return render(request, 'privacy_policy.html')
 
-
+# def about(request):
+#   return render(request, 'about.html')
 # def custom_404_view(request, exception):
-  #  return render(request, '404.html', status=404)
-
+#  return render(request, '404.html', status=404)
